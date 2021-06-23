b9942e4e6ef7c8324b1e79776dd997cdda372fe6
Tijani Fulani <Fulani07@gmail.com>
2021-06-23 15:14:49 +0100

    Main and form set

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,27 @@
 import { useState } from "react";
 
+import AnswersList from "./AnswersList";
+import Form from "./Form";
+
+const ANSWER_EXAMPLE = {
+  username: "Nicolas",
+  email: "nico@nico.las",
+  logo: "4",
+  colour: "2",
+  consistency: "3",
+  timeSpent: ["swimming", "chatting"],
+  review: "I love my rubber ducky! ❤️",
+  bestFeatures: ["colour", "size"],
+  worstFeatures: ["sound"]
+};
+
 function Main() {
   const [open, setOpen] = useState(false); //Ignore this state
+  const [answersList, setAnswerList] = useState([]);
+
+  function addAnswer(answers) {
+    setAnswerList([...answersList, answers]);
+  }
 
   return (
     <main className="main">
@@ -9,7 +29,9 @@ function Main() {
         <h2>Answers list</h2>
         <AnswersList answersList={answersList} />
       </section>
-      <section className="main__form">{/* a form should be here */}</section>
+      <section className="main__form">
+        <Form addAnswer={addAnswer} />
+      </section>
     </main>
   );
 }
